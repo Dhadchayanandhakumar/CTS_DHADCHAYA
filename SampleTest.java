22ef4af8ebb5ef9100622f9f527c3e31f7066d1c
Dhadchayanandhakumar <dhadchu5@gmail.com>
2026-06-24 22:48:56 +0530

    Add files via upload

---
+++ SampleTest.java
@@ -0,0 +1,11 @@
+package junit_exercises;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+    @Test
+    public void testMethod() {
+        System.out.println("JUnit is working successfully!");
+    }
+}
